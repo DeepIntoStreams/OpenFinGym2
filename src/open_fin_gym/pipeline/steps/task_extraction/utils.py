3a2a61963d58d2cae018479b02f8a62bc20311e1
JLanghamLopez <jlanghamlopez@turing.ac.uk>
2026-07-02 19:53:07 +0100

    Fix db commit (#18)

--- src/open_fin_gym/pipeline/steps/task_extraction/utils.py
+++ src/open_fin_gym/pipeline/steps/task_extraction/utils.py
@@ -1,3 +1,5 @@
+from typing import Type
+
 from open_fin_gym.pipeline.db.tables import (
     GroundTruthDatasetCandidate,
     MetricCandidate,
@@ -7,33 +9,33 @@ from open_fin_gym.pipeline.db.tables import (
     TrainDatasetCandidate,
 )
 
+from .prompts import Dataset
 
-def unpack_datasets(
-    task_candidate: TaskCandidate,
-) -> list[
+type DatasetType = (
     TrainDatasetCandidate
     | TestInputDatasetCandidate
     | TestOutputDatasetCandidate
     | GroundTruthDatasetCandidate
-]:
-    training_data = [
-        TrainDatasetCandidate(**x.model_dump(), task_candidate=task_candidate)
-        for x in task_candidate.training_data
-    ]
-    test_input = [
-        TestInputDatasetCandidate(**x.model_dump(), task_candidate=task_candidate)
-        for x in task_candidate.test_input
-    ]
-    test_output = [
-        TestOutputDatasetCandidate(**x.model_dump(), task_candidate=task_candidate)
-        for x in task_candidate.test_output
-    ]
-    ground_truth_data = [
-        GroundTruthDatasetCandidate(**x.model_dump(), task_candidate=task_candidate)
-        for x in task_candidate.ground_truth_data
-    ]
+)
 
-    return training_data + test_input + test_output + ground_truth_data
+
+def unpack_datasets(
+    dataset_type: Type[DatasetType],
+    task_candidate: TaskCandidate,
+    datasets: list[Dataset],
+) -> list[DatasetType]:
+
+    return [
+        dataset_type(
+            name=x.name,
+            filename=x.filename,
+            description=x.description,
+            source=x.source,
+            relevant_urls=x.relevant_urls,
+            task_candidate=task_candidate,
+        )
+        for x in datasets
+    ]
 
 
 def unpack_metrics(task_candidate: TaskCandidate) -> list[MetricCandidate]:
