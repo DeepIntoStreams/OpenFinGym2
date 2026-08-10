1ba34257867ab26dfc3c2d6469e2387ca53ae787
Cole Looney <colelooney22@gmail.com>
2026-08-10 02:45:24 -0500

    Add explicit data-availability criterion to judge (#32) (#43)

--- src/open_fin_gym/pipeline/db/tables.py
+++ src/open_fin_gym/pipeline/db/tables.py
@@ -72,6 +72,7 @@ class RejectionReason(StrEnum):
     JudgeRejected = "judge_rejected"
     JudgeCutoff = "judge_cutoff"
     LLMError = "llm_error"
+    DataNotPublic = "data_not_public"
 
 
 class Paper(Base):
